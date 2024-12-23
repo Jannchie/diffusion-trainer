3827978948421468aa011015dadd544397803e9b
Jannchie <jannchie@gmail.com>
2024-12-23 22:58:53 +0900

    refactor(script): add progress tracking and file handling improvements

--- scripts/convert_ss_meta_to_trainer_meta.py
+++ scripts/convert_ss_meta_to_trainer_meta.py
@@ -1,10 +1,11 @@
-import json
 import argparse
+import json
+from contextlib import suppress
 from pathlib import Path
 
-# ss_meta_path
-# ss_latent_path
-# trainer_meta_path
+from diffusion_trainer.shared import get_progress
+
+progress = get_progress()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--ss_meta_path", type=str, required=True)
@@ -24,22 +25,18 @@ trainer_meta_path.mkdir(exist_ok=True)
 (trainer_meta_path / "tags").mkdir(exist_ok=True)
 (trainer_meta_path / "captions").mkdir(exist_ok=True)
 
+with progress:
+    for key in progress.track(ss_meta.keys(), description="Converting metadata"):
+        with suppress(FileNotFoundError):
+            npz_path = ss_latent_path / f"{key}.npz"
 
-for key in ss_meta:
-    try:
-        print(key)
-        npz_path = ss_latent_path / f"{key}.npz"
+            target_npz_path = trainer_meta_path / "latents" / f"{key}.npz"
+            target_npz_path.write_bytes(npz_path.read_bytes())
 
-        target_npz_path = trainer_meta_path / "latents" / f"{key}.npz"
-        target_npz_path.write_bytes(npz_path.read_bytes())
+            if tags := ss_meta[key].get("tags", None):
+                target_tags_path = trainer_meta_path / "tags" / f"{key}.txt"
+                target_tags_path.write_text(tags)
 
-        if tags := ss_meta[key].get("tags", None):
-            target_tags_path = trainer_meta_path / "tags" / f"{key}.txt"
-            target_tags_path.write_text(tags)
-
-        if prompt := ss_meta[key].get("caption", None):
-            target_prompt_path = trainer_meta_path / "caption" / f"{key}.txt"
-            target_prompt_path.write_text(prompt)
-
-    except Exception as e:
-        continue
+            if prompt := ss_meta[key].get("caption", None):
+                target_prompt_path = trainer_meta_path / "caption" / f"{key}.txt"
+                target_prompt_path.write_text(prompt)
