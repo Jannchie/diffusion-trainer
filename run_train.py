7783f3291ba06876f3c67866bc676130f5b54203
Jannchie <jannchie@gmail.com>
2024-12-28 17:16:23 +0900

    feat(training): use argparse for config path && add simple_train script

--- run_train.py
+++ run_train.py
@@ -8,12 +8,21 @@ logging.basicConfig(level=logging.INFO, format="%(message)s", datefmt="[%X]", ha
 
 if __name__ == "__main__":
     import tomllib
+    from argparse import ArgumentParser
     from pathlib import Path
 
     from diffusion_trainer.config import SDXLConfig
     from diffusion_trainer.finetune.sdxl import SDXLTuner
 
-    config_path = Path("configs/sdxl_lokr.toml")
+    logger = logging.getLogger(__name__)
+
+    parser = ArgumentParser()
+    parser.add_argument("--config", type=str)
+    args = parser.parse_args()
+
+    logger.info('Loading config from "%s"', args.config)
+
+    config_path = Path(args.config)
     sdxl_config_dict = tomllib.load(config_path.open("rb"))
     sdxl_config = SDXLConfig(**sdxl_config_dict)
     tuner = SDXLTuner(sdxl_config)
